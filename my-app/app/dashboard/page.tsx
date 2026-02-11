7698c11b9f391b12575d48d4fec68d0b0e5b3edd
natale565 <natale565@icloud.com>
2026-02-10 23:14:57 -0500

    added next.js, login page

---
+++ my-app/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+export default function Dashboard() {
+  return (
+    <div>
+
+      
+      <h1>Login Page</h1>
+    </div>
+  );
+}
---
+++ my-app/app/page.tsx
@@ -0,0 +1,80 @@
+'use client';
+
+import * as CUR from '@chakra-ui/react';
+import users from '@/data/mockUsers.json';
+import { useState } from 'react';
+
+export default function Home() {
+  const [authedUsers, setAuthedUsers] = useState(users);
+  const [input, setInput] = useState({ userName: '', password: '' });
+  const [authenticated, setAuthenticated] = useState(false);
+
+  const handleUserLogin = (userName: string, password: string) => {
+    const existingUser = authedUsers.some(
+      (foundUser) => foundUser.userName === userName,
+    );
+    if (!existingUser) {
+      return (
+        setAuthenticated(false),
+        alert('Access denied: invalid username')
+      );
+    }
+    const matchingPassword = authedUsers.some(
+      (foundUser) => foundUser.password === password,
+    );
+    if (!matchingPassword) {
+      return (
+        setAuthenticated(false),
+        alert('Access denied: incorrect password')
+      );
+    }
+
+    setAuthenticated(true);
+    alert('Login successful!');
+  };
+
+  //create link to dashboard
+  return (
+    <CUR.AbsoluteCenter>
+      <CUR.Card.Root h={'lg'} w={'lg'}>
+        <CUR.Card.Body>
+          Welcome
+          <form action="submit">
+            <CUR.Fieldset.Root>
+              <CUR.Fieldset.Content>
+                <CUR.Field.Root>
+                  <CUR.Field.Label>User ID</CUR.Field.Label>
+                  <CUR.Input
+                    type="text"
+                    bg={''}
+                    value={input.userName}
+                    onChange={(e) => {
+                      setInput({ ...input, userName: e.target.value });
+                    }}
+                  />
+                </CUR.Field.Root>
+                <CUR.Field.Root>
+                  <CUR.Field.Label>User Password</CUR.Field.Label>
+                  <CUR.Input
+                    type="password"
+                    bg={''}
+                    value={input.password}
+                    onChange={(e) => {
+                      setInput({ ...input, password: e.target.value });
+                    }}
+                  />
+                </CUR.Field.Root>
+
+                <CUR.Button onClick={handleUserLogin}>
+                  <CUR.Link to={'/dashboard'}>Login</CUR.Link>
+                </CUR.Button>
+              </CUR.Fieldset.Content>
+            </CUR.Fieldset.Root>
+          </form>
+        </CUR.Card.Body>
+      </CUR.Card.Root>
+    </CUR.AbsoluteCenter>
+  );
+}
+
+//commit current changes
